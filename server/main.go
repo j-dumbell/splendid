614547f892139ddbfde5b4664325161122f90f69
j-dumbell <51704133+j-dumbell@users.noreply.github.com>
2020-07-06 22:13:45 +0100

    Dockerize go (#5)
    Co-authored-by: James Dumbell <jamesd@Jamess-MacBook-Pro.local>

--- server/main.go
+++ server/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	port := 8000
+	port := 8080
 	fmt.Println("Starting on port " + strconv.Itoa(port))
 	http.HandleFunc("/health", routes.Health)
 	http.ListenAndServe(":" + strconv.Itoa(port), nil)
