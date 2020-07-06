38e02e369b84a7996ffa1fd45da3beac6f2991d1
Van Le <vannio@users.noreply.github.com>
2020-07-06 20:45:28 +0100

    Cleanup

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders the title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Splendid/i);
   expect(linkElement).toBeInTheDocument();
 });
