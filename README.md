1b3662d38dadca98453fb23b7e80bb753ac29ee1
Van <notify@vann.io>
2020-07-08 18:33:11 +0100

    Websocket client (#13)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# splendid
+# Splendid
+
+A bootleg version of the board game, Splendor
---
+++ client/src/hooks/websocket.ts
@@ -0,0 +1,50 @@
+import { useRef, useEffect, useState } from "react";
+
+import config from "../config";
+
+export const useWebSocket = (path: string) => {
+  const ws = useRef<WebSocket>();
+  const [error, setError] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [message, setMessage] = useState<any>();
+
+  useEffect(() => {
+    if (!config.apiUrl) {
+      const errorMessage = "apiUrl not set";
+      setError(errorMessage);
+      return;
+    }
+
+    const url = new URL(`wss://${config.apiUrl}${path}`);
+    ws.current = new WebSocket(url.toString());
+
+    setLoading(true);
+    if (ws.current) {
+      ws.current.onopen = () => {
+        const status = "connection opened";
+        setMessage(status);
+        setLoading(false);
+      };
+
+      ws.current.onclose = () => {
+        const status = "connection closed";
+        setMessage(status);
+      };
+
+      ws.current.onmessage = ({ data }) => {
+        console.log("message received", data);
+        let parsed: any;
+        try {
+          parsed = JSON.parse(data);
+        } catch (e) {
+          parsed = data;
+        }
+        setMessage(parsed);
+      };
+    }
+
+    return () => ws.current && ws.current.close();
+  }, [path]);
+
+  return [loading, error, message];
+};
