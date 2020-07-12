38e155ee551819b5e1537ddfd8f630e6854d2a82
James Dumbell <jdumbell92@gmail.com>
2020-07-12 18:20:11 +0100

    fix import

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	//"net/http"
-	//"strconv"
+	"net/http"
+	"github.com/j-dumbell/splendid/server/api"
+	"strconv"
 
-	//"golang.org/x/net/websocket"
-	"github.com/j-dumbell/splendid/server/pkg/splendid"
+	"golang.org/x/net/websocket"
 )
 
 func main() {
-	//port := 8080
-	//fmt.Println("Starting on port " + strconv.Itoa(port))
-	//http.HandleFunc("/health", api.Health)
-	//http.Handle("/", websocket.Handler(api.WebSocket))
-	//http.ListenAndServe(":"+strconv.Itoa(port), nil)
-
-	fmt.Println(splendid.Card1)
+	port := 8080
+	fmt.Println("Starting on port " + strconv.Itoa(port))
+	http.HandleFunc("/health", api.Health)
+	http.Handle("/", websocket.Handler(api.WebSocket))
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
 }
