89c19631b03bd72e35fcf4c0918c1269b223df23
Van Le <vannio@users.noreply.github.com>
2020-07-12 18:21:58 +0100

    Format imports

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/j-dumbell/splendid/server/api"
 	"strconv"
+
+	"github.com/j-dumbell/splendid/server/api"
 
 	"golang.org/x/net/websocket"
 )
