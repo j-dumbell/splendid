cbe11d838bf17a9a493a4c3e49eae2df0299c3e3
j-dumbell <51704133+j-dumbell@users.noreply.github.com>
2020-07-18 17:49:04 +0100

    Shuffle fn & test (#22)

--- server/pkg/splendid/board.go
+++ server/pkg/splendid/board.go
@@ -1,9 +1,27 @@
 package splendid
 
+import (
+	"math/rand"
+	"reflect"
+)
+
 type Board struct {
 	Deck1  []Card
 	Deck2  []Card
 	Deck3  []Card
 	Elites []Elite
 	Bank   map[Resource]int
+}
+
+func Shuffle(arr interface{}, seed int64) interface{} {
+	arrType := reflect.TypeOf(arr)
+	arrValue := reflect.ValueOf(arr)
+	arrLength := arrValue.Len()
+	arrRand := reflect.MakeSlice(arrType, 0, 0)
+	r := rand.New(rand.NewSource(seed))
+	perms := r.Perm(arrLength)
+	for _, v := range perms {
+		arrRand = reflect.Append(arrRand, arrValue.Index(v))
+	}
+	return arrRand.Interface()
 }
