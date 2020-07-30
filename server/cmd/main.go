e7167c38ed14e6881ff6651b7504a2da34dcb2b6
James Dumbell <jdumbell92@gmail.com>
2020-07-30 13:10:40 +0200

    fix main

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	//"fmt"
-	//"net/http"
+	"net/http"
+	"strconv"
 	"fmt"
-	"github.com/j-dumbell/splendid/server/pkg/splendid"
-	//"strconv"
+	"golang.org/x/net/websocket"
 
-	//"github.com/j-dumbell/splendid/server/api"
-	//
-	//"golang.org/x/net/websocket"
+	"github.com/j-dumbell/splendid/server/api"
 )
 
 func main() {
-	//port := 8080
-	//fmt.Println("Starting on port " + strconv.Itoa(port))
-	//http.HandleFunc("/health", api.Health)
-	//http.Handle("/", websocket.Handler(api.WebSocket))
-	//http.ListenAndServe(":"+strconv.Itoa(port), nil)
-
-	_, err := splendid.ReadCards("./config/cards.csv")
-	if err != nil {
-		fmt.Println(err)
-	}
+	port := 8080
+	fmt.Println("Starting on port " + strconv.Itoa(port))
+	http.HandleFunc("/health", api.Health)
+	http.Handle("/", websocket.Handler(api.WebSocket))
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
 }
