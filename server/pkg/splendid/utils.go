c33bec9f2fc9d9c9aded42391a8fc0a8ee3f0c0d
Van Le <vannio@users.noreply.github.com>
2020-07-31 09:54:54 +0100

    And another

--- server/pkg/splendid/utils.go
+++ server/pkg/splendid/utils.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 )
 
+// ReadCSV parses and returns a CSV file from a given path
 func ReadCSV(path string) ([][]string, error) {
 	csvfile, err := os.Open(path)
 	if err != nil {
