219aa38aa92d83892c86bdb7dff531299d263db5
Van Le <vannio@users.noreply.github.com>
2020-07-31 21:03:38 +0100

    Add config

---
+++ server/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+// CardsCSVPath is the relative location for the CSV to populate Cards
+var CardsCSVPath string = ""
+
+// ElitesCSVPath is the relative location for the CSV to populate Elites
+var ElitesCSVPath string = ""
+
+// ResourceDefault is the default amount for each Resource
+var ResourceDefault int = 6
