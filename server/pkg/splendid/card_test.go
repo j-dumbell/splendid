b726629d8cadc61716dad3d660b4cb53f4b5524a
Van Le <vannio@users.noreply.github.com>
2020-07-31 09:37:34 +0100

    Add create card test

--- server/pkg/splendid/card_test.go
+++ server/pkg/splendid/card_test.go
@@ -8,25 +8,25 @@ import (
 func TestFilterCards(t *testing.T) {
 	cards := []Card{
 		Card{
-			Id: 1,
-			Tier: 1,
-			Points: 10,
-			Cost: map[Resource]int{Blue: 1},
-			Income: Black,
+			ID:       1,
+			Tier:     1,
+			Points:   10,
+			Cost:     map[Resource]int{Blue: 1},
+			Income:   Black,
 			IsPublic: true,
 		},
 		Card{
-			Id: 1,
-			Tier: 2,
-			Points: 10,
-			Cost: map[Resource]int{Blue: 1},
-			Income: Green,
+			ID:       1,
+			Tier:     2,
+			Points:   10,
+			Cost:     map[Resource]int{Blue: 1},
+			Income:   Green,
 			IsPublic: true,
 		},
 	}
 
 	f := func(c Card) bool {
-		return c.Tier==1
+		return c.Tier == 1
 	}
 
 	if !reflect.DeepEqual([]Card{cards[0]}, FilterCards(cards, f)) {
@@ -34,6 +34,29 @@ func TestFilterCards(t *testing.T) {
 	}
 
 	if reflect.DeepEqual([]Card{cards[1]}, FilterCards(cards, f)) {
+		t.Fail()
+	}
+}
+
+func TestCreateCard(t *testing.T) {
+	csvRow := []string{"1", "Black", "0", "0", "1", "1", "1", "1"}
+
+	expectedCard := Card{
+		ID:     1,
+		Tier:   1,
+		Points: 0,
+		Cost: map[Resource]int{
+			Black: 0,
+			White: 1,
+			Red:   1,
+			Blue:  1,
+			Green: 1,
+		},
+		Income:   MapResource("Black"),
+		IsPublic: false,
+	}
+
+	if !reflect.DeepEqual(CreateCard(csvRow, 0), expectedCard) {
 		t.Fail()
 	}
 }
