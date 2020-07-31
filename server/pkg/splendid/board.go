ea6dc51c7cf935a871ca8eac4e4c31323787b294
James Dumbell <jdumbell92@gmail.com>
2020-07-31 21:30:41 +0100

    fix param names

--- server/pkg/splendid/board.go
+++ server/pkg/splendid/board.go
@@ -17,13 +17,13 @@ func FilterFn(tier int) func(c Card) bool {
 }
 
 func CreateDecks(cardsPath string, elitesPath string) ([]Card, []Card, []Card, []Elite) {
-	cardRows, _ := util.ReadCSV(cardPath)
+	cardRows, _ := util.ReadCSV(cardsPath)
 	cards := CreateCards(cardRows)
 	deck1 := FilterCards(cards, FilterFn(1))
 	deck2 := FilterCards(cards, FilterFn(2))
 	deck3 := FilterCards(cards, FilterFn(3))
 
-	eliteRows, _ := util.ReadCSV(elitePath)
+	eliteRows, _ := util.ReadCSV(elitesPath)
 	elites := CreateElites(eliteRows)
 
 	return deck1, deck2, deck3, elites
