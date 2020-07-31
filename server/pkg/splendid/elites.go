713a992b9bede260b53189344cf3da1a24238cb6
James Dumbell <jdumbell92@gmail.com>
2020-07-31 20:08:52 +0100

    add csv & test

--- server/pkg/splendid/elites.go
+++ server/pkg/splendid/elites.go
@@ -1,6 +1,26 @@
 package splendid
 
 type Elite struct {
+	ID	   int
 	Points int
 	Cost   map[Resource]int
+}
+
+func CreateElites(rows [][]string) []Elite {
+	var elites []Elite
+	for i, v := range rows {
+		elite := Elite{
+			ID: i+1,
+			Points: StringToInt(v[0]),
+			Cost: map[Resource]int{
+				Black: StringToInt(v[1]),
+				White: StringToInt(v[2]),
+				Red: StringToInt(v[3]),
+				Blue: StringToInt(v[4]),
+				Green: StringToInt(v[5]),
+			},
+		}
+		elites = append(elites, elite)
+	}
+	return elites
 }
---
+++ server/pkg/splendid/elites_test.go
@@ -0,0 +1,28 @@
+package splendid
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCreateElites(t *testing.T) {
+	rows := [][]string{
+		[]string{"3", "0", "0", "4", "0", "4"},
+		[]string{"3", "3", "3", "3", "0", "0"},
+	}
+	expected := []Elite{
+		Elite{
+			ID:     1,
+			Points: 3,
+			Cost:   map[Resource]int{Black: 0, White: 0, Red: 4, Blue: 0, Green: 4},
+		},
+		Elite{
+			ID:     2,
+			Points: 3,
+			Cost:   map[Resource]int{Black: 3, White: 3, Red: 3, Blue: 0, Green: 0},
+		},
+	}
+	if !reflect.DeepEqual(CreateElites(rows), expected) {
+		t.Fail()
+	}
+}
