4473289a06ccd9d1001c20747a38c81a3edfe8b9
Van Le <vannio@users.noreply.github.com>
2020-08-02 12:27:14 +0100

    Comment cleanup

--- server/pkg/splendid/resource.go
+++ server/pkg/splendid/resource.go
@@ -5,22 +5,11 @@ type Resource struct {
 	Name string
 }
 
-// Black is a type of Resource
 var Black = Resource{Name: "Black"}
-
-// White is a type of Resource
 var White = Resource{Name: "White"}
-
-// Red is a type of Resource
 var Red = Resource{Name: "Red"}
-
-// Blue is a type of Resource
 var Blue = Resource{Name: "Blue"}
-
-// Green is a type of Resource
 var Green = Resource{Name: "Green"}
-
-// Yellow is a type of Resource
 var Yellow = Resource{Name: "Yellow"}
 
 // MapResource maps a string to a Resource
