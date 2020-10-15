8c457e5364e7be415b22c4e647c3374a5cc31aab
James Dumbell <jdumbell92@gmail.com>
2020-10-15 13:49:46 +0100

    remove dead func

--- server/pkg/splendid/player.go
+++ server/pkg/splendid/player.go
@@ -7,12 +7,3 @@ type Player struct {
 	ReservedHand []Card
 	Bank         map[Resource]int
 }
-
-func NewPlayer(name string) Player {
-	return Player{
-		Name:         name,
-		ActiveHand:   []Card{},
-		ReservedHand: []Card{},
-		Bank:         map[Resource]int{Black: 0, White: 0, Red: 0, Blue: 0, Green: 0, Yellow: 0},
-	}
-}
