a6807bde00c9406e827c4839fea3cbea9a2be829
Van <notify@vann.io>
2020-10-27 21:47:05 +0000

    Read pump (#58)
    Co-authored-by: Van Le <vannio@users.noreply.github.com>

--- server/api/client.go
+++ server/api/client.go
@@ -1,9 +1,30 @@
 package api
 
-import "golang.org/x/net/websocket"
+import (
+	"fmt"
+
+	"golang.org/x/net/websocket"
+)
 
 type Client struct {
 	lobby *Lobby
-	conn *websocket.Conn
-	send chan Response
+	conn  *websocket.Conn
+	send  chan Response
+}
+
+func (c *Client) ReadPump() {
+	defer func() {
+		c.lobby.leave <- c
+		c.conn.Close()
+	}()
+
+	for {
+		var p Payload
+		err := websocket.JSON.Receive(c.conn, &p)
+		if err != nil {
+			fmt.Printf("ws read error: %v", err)
+			break
+		}
+		c.lobby.broadcast <- p
+	}
 }
