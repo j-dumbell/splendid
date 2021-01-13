f6b100ec119c67e820c3774bb9dbb1ccf9a17494
Van Le <vannio@users.noreply.github.com>
2021-01-13 22:09:00 +0000

    single cased codes

--- server/pkg/util/rand_id.go
+++ server/pkg/util/rand_id.go
@@ -7,7 +7,7 @@ import (
 // RandID generates a random alphanumeric string of length n
 func RandID(n int, seed int64) string {
 	r := rand.New(rand.NewSource(seed))
-	runes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	runes := []rune("abcdefghijklmnopqrstuvwxyz0123456789")
 	id := make([]rune, n)
 	for i := range id {
 		id[i] = runes[r.Intn(len(runes))]
