0cecf741171c277fe782d9f66e724912a1610359
Van Le <vannio@users.noreply.github.com>
2021-01-30 17:06:35 +0000

    fix test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-it('renders the title', () => {
-  const { getByText } = render(<App />);
+import rootReducer from "./state/reducer";
+import App from "./App";
+
+const store = createStore(rootReducer);
+
+it("renders the title", () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = getByText(/Splendid/i);
   expect(linkElement).toBeInTheDocument();
 });
