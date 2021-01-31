e9377ae6d117e756cf8ca5c1111b9c916b2ebf19
vannio <vannio@users.noreply.github.com>
2021-01-31 19:14:38 +0000

    newlines

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -11,16 +11,16 @@ resource "aws_iam_user" "ci_service" {
 }
 
 resource "aws_iam_user_policy_attachment" "van-policy-attachment" {
-  user = aws_iam_user.van.name
+  user       = aws_iam_user.van.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_user_policy_attachment" "james-policy-attachment" {
-  user = aws_iam_user.james.name
+  user       = aws_iam_user.james.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_user_policy_attachment" "ci-service-policy-attachment" {
-  user = aws_iam_user.ci_service.name
+  user       = aws_iam_user.ci_service.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
