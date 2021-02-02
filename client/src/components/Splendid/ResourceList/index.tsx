dfdd59080c8d6593183a78169d56e0cf65ff791c
vannio <vannio@users.noreply.github.com>
2021-02-02 22:15:59 +0000

    form

--- client/src/components/Splendid/ResourceList/index.tsx
+++ client/src/components/Splendid/ResourceList/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Formik, Form, Field } from "formik";
 
 import { splendidResource, SplendidCard } from "../domain";
 import { UnstyledButton } from "../../common/Button";
 import ResourceCount from "./ResourceCount";
+// import { sendJSON } from "../../../hooks/useWebsocket";
 
 type Props = {
   resourceList: Record<string, number>;
@@ -56,31 +58,40 @@ export const PlayerResourceList = ({
   </>
 );
 
-export const BoardResourceList = ({
-  resourceList,
-  hideEmpty,
-  purchased,
-}: Props) => (
-  <>
-    {splendidResource.map((resource, i) => {
-      const purchasedCount =
-        purchased?.filter((card) => card.income === resource).length ?? 0;
-      const totals = (resourceList[resource] ?? 0) + purchasedCount;
-      if (totals <= 0 && hideEmpty) {
-        return null;
-      }
-      return (
-        <UnstyledButton>
-          <ResourceCount
+export const BoardResourceList = ({ resourceList }: Props) => (
+  <Formik
+    initialValues={{ id: "" }}
+    onSubmit={() => {}}
+    // onSubmit={(values, { resetForm, setSubmitting }) => {
+    //   sendJSON({
+    //     action: "game",
+    //     params: { ...values, gameAction: "takeResources" },
+    //   });
+    //   resetForm();
+    //   setSubmitting(false);
+    // }}
+  >
+    <Form>
+      {splendidResource.map((resource, i) => {
+        return (
+          <UnstyledButton
+            type="button"
             key={`resource-${i}`}
-            resource={resource}
-            count={resourceList[resource]}
-            purchasedCount={purchasedCount}
-          />
-        </UnstyledButton>
-      );
-    })}
-  </>
+            onClick={(e) => console.log(e.currentTarget.id)}
+            id={resource}
+          >
+            <Field
+              type="hidden"
+              name={resource}
+              placeholder={resource}
+              required
+            />
+            <ResourceCount resource={resource} count={resourceList[resource]} />
+          </UnstyledButton>
+        );
+      })}
+    </Form>
+  </Formik>
 );
 
 export default ResourceList;
