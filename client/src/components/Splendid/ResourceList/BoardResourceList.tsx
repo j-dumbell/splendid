32a5e7788998d4d52d0b05e8fafacf3b830579f9
Van <notify@vann.io>
2021-02-06 17:00:00 +0000

    Disable buying yellow (#94)
    Co-authored-by: vannio <vannio@users.noreply.github.com>

--- client/src/components/Splendid/ResourceList/BoardResourceList.tsx
+++ client/src/components/Splendid/ResourceList/BoardResourceList.tsx
@@ -11,10 +11,9 @@ import { ResourceListProps } from ".";
 export const BoardResourceList = ({ resourceList }: ResourceListProps) => (
   <Formik
     validate={validateMax}
-    initialValues={Object.keys(resourceList).reduce(
-      (prev, next) => ({ ...prev, [next]: 0 }),
-      {}
-    )}
+    initialValues={Object.keys(resourceList)
+      .filter((resource) => resource !== "yellow")
+      .reduce((prev, next) => ({ ...prev, [next]: 0 }), {})}
     onSubmit={(values) =>
       sendJSON({
         action: "game",
@@ -26,38 +25,50 @@ export const BoardResourceList = ({ resourceList }: ResourceListProps) => (
       <Form>
         {splendidResource.map((resource, i) => (
           <FlexContainer key={i}>
-            <div>
-              <button
-                disabled={values[resource] <= 0}
-                type="button"
-                value={resource}
-                onClick={async ({ currentTarget: { value } }) => {
-                  setFieldValue(value, values[value] - 1);
-                }}
-              >
-                -
-              </button>
-              <button
-                disabled={Boolean(errors[resource])}
-                type="button"
-                value={resource}
-                onClick={async ({ currentTarget: { value } }) => {
-                  setFieldValue(value, values[value] + 1);
-                }}
-              >
-                +
-              </button>
-              <Field
-                style={{ marginLeft: "5px", marginRight: "5px" }}
-                type="text"
-                name={resource}
-                disabled
-                value={values[resource]}
-                id={resource}
-                size={6}
+            {resource === "yellow" ? (
+              <ResourceCount
+                resource={resource}
+                count={resourceList[resource]}
               />
-            </div>
-            <ResourceCount resource={resource} count={resourceList[resource]} />
+            ) : (
+              <>
+                <ResourceCount
+                  resource={resource}
+                  count={resourceList[resource]}
+                />
+                <FlexContainer>
+                  <button
+                    disabled={values[resource] <= 0}
+                    type="button"
+                    value={resource}
+                    onClick={async ({ currentTarget: { value } }) => {
+                      setFieldValue(value, values[value] - 1);
+                    }}
+                  >
+                    -
+                  </button>
+                  <button
+                    disabled={Boolean(errors[resource])}
+                    type="button"
+                    value={resource}
+                    onClick={async ({ currentTarget: { value } }) => {
+                      setFieldValue(value, values[value] + 1);
+                    }}
+                  >
+                    +
+                  </button>
+                  <Field
+                    style={{ marginLeft: "5px", marginRight: "5px" }}
+                    type="text"
+                    name={resource}
+                    disabled
+                    value={values[resource]}
+                    id={resource}
+                    size={6}
+                  />
+                </FlexContainer>
+              </>
+            )}
           </FlexContainer>
         ))}
         <button type="submit" disabled={Object.values(errors).some(Boolean)}>
