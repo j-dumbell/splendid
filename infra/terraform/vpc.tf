5c37d49402faa4603c5085a7953ac5677b4b6618
j-dumbell <51704133+j-dumbell@users.noreply.github.com>
2021-02-08 16:16:26 +0000

    Add VPC resources (#90)

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -1,6 +1,48 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "game-vpc"
+    Name = "game"
+  }
+}
+
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "game"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  depends_on = [aws_internet_gateway.ig]
+  tags = {
+    Name = "game"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "game"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-game"
   }
 }
