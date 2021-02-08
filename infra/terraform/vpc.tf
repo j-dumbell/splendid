f3cc65912e1211328563a634a5a187c6717b221a
James Dumbell <jdumbell92@gmail.com>
2021-02-08 16:11:03 +0000

    baseline vpc working

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -1,50 +1,48 @@
-resource "aws_vpc" "game_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "game"
   }
 }
 
-resource "aws_subnet" "game_public_subnet" {
-  vpc_id     = aws_vpc.game_vpc.id
-  cidr_block = "10.0.0.0/24"
-
-  tags = {
-    Name = "public-game"
-  }
-}
-
-resource "aws_subnet" "game_private_subnet" {
-  vpc_id     = aws_vpc.game_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "private-game"
-  }
-}
-
-resource "aws_internet_gateway" "game_igw" {
-  vpc_id = aws_vpc.game_vpc.id
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "game"
   }
 }
 
-resource "aws_nat_gateway" "game_ngw" {
-  allocation_id = aws_eip.game_ngw_eip.id
-  subnet_id     = aws_subnet.game_public_subnet.id
-
-  depends_on = [aws_internet_gateway.game_igw]
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
 }
 
-resource "aws_network_interface" "game_interface" {
-  subnet_id   = aws_subnet.game_public_subnet.id
-  private_ips = ["10.0.0.10"]
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  depends_on = [aws_internet_gateway.ig]
+  tags = {
+    Name = "game"
+  }
 }
 
-resource "aws_eip" "game_ngw_eip" {
-  vpc                       = true
-  network_interface         = aws_network_interface.game_interface.id
-  associate_with_private_ip = "10.0.0.10"
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "game"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-game"
+  }
 }
