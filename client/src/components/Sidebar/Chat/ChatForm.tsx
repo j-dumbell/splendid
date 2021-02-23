afbdc9e71a21369f0838cf20593367e1cc74e0d0
Van <notify@vann.io>
2021-02-23 23:47:51 +0000

    Chat display name (#108)
    Co-authored-by: Van Le <vannio@users.noreply.github.com>

--- client/src/components/Sidebar/Chat/ChatForm.tsx
+++ client/src/components/Sidebar/Chat/ChatForm.tsx
@@ -4,17 +4,21 @@ import { Formik } from "formik";
 import { sendJSON } from "../../../hooks/useWebsocket";
 import { useLobbyId } from "../../../hooks/useLobbyId";
 import { FlexForm, FlexField } from "../../common/FlexContainer";
+import { useSelector } from "react-redux";
+import { State } from "../../../state/domain";
 
 const SendChatForm = () => {
   const [lobbyId] = useLobbyId();
+  const clientId = useSelector(({ clientId }: State) => clientId);
+
   if (!lobbyId) {
     return null;
   }
   return (
     <Formik
       initialValues={{ message: "" }}
-      onSubmit={(values, { resetForm, setSubmitting }) => {
-        sendJSON({ action: "chat", params: values });
+      onSubmit={({ message }, { resetForm, setSubmitting }) => {
+        sendJSON({ action: "chat", params: { message, clientId } });
         resetForm();
         setSubmitting(false);
       }}
