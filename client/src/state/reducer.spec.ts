a51d3d09a0d1cac2aba9c9f7efa47a695be6f984
vannio <vannio@users.noreply.github.com>
2021-02-27 19:05:48 +0000

    Add EXIT_LOBBY tests

--- client/src/state/reducer.spec.ts
+++ client/src/state/reducer.spec.ts
@@ -1,4 +1,4 @@
-import { JoinLobbyAction, State } from "./domain";
+import { ExitLobbyAction, JoinLobbyAction, State } from "./domain";
 import reducer, { defaultState } from "./reducer";
 
 describe("reducer()", () => {
@@ -24,7 +24,7 @@ describe("reducer()", () => {
       expect(newState).toMatchObject(expectedState);
     });
 
-    it("it returns updated state for additional players joining", () => {
+    it("it returns updated state when additional players join", () => {
       const initialState: State = {
         ...defaultState,
         clientId: 1,
@@ -73,7 +73,62 @@ describe("reducer()", () => {
       expect(newState).toMatchObject(expectedState);
     });
   });
-  describe("action EXIT_LOBBY", () => {});
+
+  describe("action EXIT_LOBBY", () => {
+    it("it returns updated state on leaving a lobby", () => {
+      const initialState: State = {
+        ...defaultState,
+        clientId: 1,
+        lobbyId: "123abc",
+        playerNames: { 
+          1: "Van", 
+          2: "James"
+        },
+      };
+      const action: ExitLobbyAction = {
+        type: "EXIT_LOBBY",
+        payload: {
+          lobbyId: "456def",
+          clientId: 1,
+          playerNames: { 2: "James" },
+        },
+      };
+      const newState = reducer(initialState, action);
+      const expectedState = {
+        ...initialState,
+        playerNames: {},
+        lobbyId: undefined,
+      };
+      expect(newState).toMatchObject(expectedState);
+    });
+
+    it("it returns updated state when other players leave", () => {
+      const initialState: State = {
+        ...defaultState,
+        clientId: 1,
+        lobbyId: "123abc",
+        playerNames: { 
+          1: "Van", 
+          2: "James"
+        },
+      };
+      const action: ExitLobbyAction = {
+        type: "EXIT_LOBBY",
+        payload: {
+          lobbyId: "456def",
+          clientId: 1,
+          playerNames: { 1: "Van" },
+        },
+      };
+      const newState = reducer(initialState, action);
+      const expectedState = {
+        ...initialState,
+        playerNames: action.payload.playerNames,
+      };
+      expect(newState).toMatchObject(expectedState);
+    });
+  });
+  
   describe("action ADD_CHAT_MESSAGE", () => {});
   describe("action ADD_HISTORY_ACTION", () => {});
   describe("action UPDATE_GAME", () => {});
