301bf30146a85559023448cf124f197ba86b4563
vannio <vannio@users.noreply.github.com>
2021-02-27 23:04:12 +0000

    Use action creators

--- client/src/state/reducer.spec.ts
+++ client/src/state/reducer.spec.ts
@@ -1,12 +1,15 @@
-import { SplendidGame } from "../components/Splendid/domain";
 import {
-  ExitLobbyAction,
-  JoinLobbyAction,
-  MessageAction,
-  SplendidAction,
-  SplendidResourceAction,
-  State,
-} from "./domain";
+  SplendidGame,
+  SplendidResourceList,
+} from "../components/Splendid/domain";
+import {
+  addChatMessage,
+  exitLobby,
+  joinLobby,
+  updateBankResources,
+  updateSplendidGame,
+} from "./actionCreator";
+import { State } from "./domain";
 import reducer, { defaultState } from "./reducer";
 
 const game: SplendidGame = {
@@ -34,24 +37,12 @@ const game: SplendidGame = {
 describe("reducer()", () => {
   describe("action JOIN_LOBBY", () => {
     it("it returns updated state on initial action", () => {
-      const action: JoinLobbyAction = {
-        type: "JOIN_LOBBY",
-        payload: {
-          lobbyId: "123abc",
-          clientId: 1,
-          playerNames: {
-            1: "Van",
-          },
-        },
-      };
+      const action = joinLobby("123abc", 1, { 1: "Van" });
       const newState = reducer(defaultState, action);
-      const expectedState: State = {
-        ...defaultState,
-        clientId: action.payload.clientId,
-        lobbyId: action.payload.lobbyId,
-        playerNames: action.payload.playerNames,
-      };
-      expect(newState).toMatchObject(expectedState);
+
+      expect(newState.clientId).toBe(action.payload.clientId);
+      expect(newState.lobbyId).toBe(action.payload.lobbyId);
+      expect(newState.playerNames).toEqual(action.payload.playerNames);
     });
 
     it("it returns updated state when additional players join", () => {
@@ -61,23 +52,13 @@ describe("reducer()", () => {
         lobbyId: "123abc",
         playerNames: { 1: "Van" },
       };
-      const action: JoinLobbyAction = {
-        type: "JOIN_LOBBY",
-        payload: {
-          lobbyId: "123abc",
-          clientId: 2,
-          playerNames: {
-            1: "Van",
-            2: "James",
-          },
-        },
-      };
+      const action = joinLobby("123abc", 2, {
+        1: "Van",
+        2: "James",
+      });
       const newState = reducer(initialState, action);
-      const expectedState: State = {
-        ...initialState,
-        playerNames: action.payload.playerNames,
-      };
-      expect(newState).toMatchObject(expectedState);
+
+      expect(newState.playerNames).toEqual(action.payload.playerNames);
     });
 
     it("it returns updated state if lobbyId is different", () => {
@@ -87,20 +68,10 @@ describe("reducer()", () => {
         lobbyId: "123abc",
         playerNames: { 1: "Van" },
       };
-      const action: JoinLobbyAction = {
-        type: "JOIN_LOBBY",
-        payload: {
-          lobbyId: "456def",
-          clientId: 1,
-          playerNames: { 1: "Van" },
-        },
-      };
+      const action = joinLobby("456def", 1, { 1: "Van" });
       const newState = reducer(initialState, action);
-      const expectedState: State = {
-        ...initialState,
-        lobbyId: action.payload.lobbyId,
-      };
-      expect(newState).toMatchObject(expectedState);
+
+      expect(newState.lobbyId).toBe(action.payload.lobbyId);
     });
   });
 
@@ -115,21 +86,11 @@ describe("reducer()", () => {
           2: "James",
         },
       };
-      const action: ExitLobbyAction = {
-        type: "EXIT_LOBBY",
-        payload: {
-          lobbyId: "456def",
-          clientId: 1,
-          playerNames: { 2: "James" },
-        },
-      };
+      const action = exitLobby(1, { 2: "James" });
       const newState = reducer(initialState, action);
-      const expectedState: State = {
-        ...initialState,
-        playerNames: {},
-        lobbyId: undefined,
-      };
-      expect(newState).toMatchObject(expectedState);
+
+      expect(newState.playerNames).toEqual({});
+      expect(newState.lobbyId).toBeUndefined();
     });
 
     it("it returns updated state when other players leave", () => {
@@ -142,40 +103,20 @@ describe("reducer()", () => {
           2: "James",
         },
       };
-      const action: ExitLobbyAction = {
-        type: "EXIT_LOBBY",
-        payload: {
-          lobbyId: "456def",
-          clientId: 1,
-          playerNames: { 1: "Van" },
-        },
-      };
+      const action = exitLobby(1, { 1: "Van" });
       const newState = reducer(initialState, action);
-      const expectedState: State = {
-        ...initialState,
-        playerNames: action.payload.playerNames,
-      };
-      expect(newState).toMatchObject(expectedState);
+
+      expect(newState.playerNames).toEqual(action.payload.playerNames);
     });
   });
 
   describe("action ADD_CHAT_MESSAGE", () => {
     it("it appends to chat state", () => {
-      const action: MessageAction = {
-        type: "ADD_CHAT_MESSAGE",
-        payload: {
-          clientId: 1,
-          timestamp: new Date(),
-          message: "hello",
-        },
-      };
+      const action = addChatMessage(1, "hello");
       const newState = reducer(defaultState, action);
       const newerState = reducer(newState, action);
-      const expectedState: State = {
-        ...defaultState,
-        chat: [action.payload, action.payload],
-      };
-      expect(newerState).toMatchObject(expectedState);
+
+      expect(newerState.chat).toEqual([action.payload, action.payload]);
     });
   });
 
@@ -183,82 +124,62 @@ describe("reducer()", () => {
 
   describe("action UPDATE_GAME", () => {
     it("it returns updated state on initial action", () => {
-      const action: SplendidAction = {
-        type: "UPDATE_GAME",
-        payload: game,
-      };
+      const action = updateSplendidGame(game);
       const newState = reducer(defaultState, action);
-      const newerState = reducer(newState, action);
-      const expectedState: State = {
-        ...defaultState,
-        game: action.payload,
-      };
-      expect(newerState).toMatchObject(expectedState);
-      expect(newerState.game?.activePlayerIndex).toBe(0);
+
+      expect(newState.game).toEqual(action.payload);
+      expect(newState.game?.activePlayerIndex).toBe(0);
     });
 
     it("it replaces updated game", () => {
-      const action: SplendidAction = {
-        type: "UPDATE_GAME",
-        payload: {
-          ...game,
-          activePlayerIndex: 1,
-        },
-      };
+      const action = updateSplendidGame({ ...game, activePlayerIndex: 1 });
       const initialState: State = { ...defaultState, game };
       const newState = reducer(initialState, action);
       const newerState = reducer(newState, action);
-      const expectedState: State = {
-        ...defaultState,
-        game: action.payload,
-      };
-      expect(newerState).toMatchObject(expectedState);
+
+      expect(newerState.game).toEqual(action.payload);
       expect(newerState.game?.activePlayerIndex).toBe(1);
     });
   });
 
   describe("action UPDATE_BANK_RESOURCE", () => {
     it("returns unchanged state if no game exists", () => {
-      const action: SplendidResourceAction = {
-        type: "UPDATE_BANK_RESOURCE",
-        payload: {},
-      };
+      const action = updateBankResources({});
       const newState = reducer(defaultState, action);
-      expect(newState).toMatchObject(defaultState);
+
+      expect(newState).toEqual(defaultState);
     });
 
     it("it inserts a blank offset bank", () => {
       const initialState: State = { ...defaultState, game };
-      const action: SplendidResourceAction = {
-        type: "UPDATE_BANK_RESOURCE",
-        payload: {},
-      };
+      const action = updateBankResources({});
       const newState = reducer(initialState, action);
-      expect(newState.game?.board.bankOffsetTemp).toMatchObject({
+      const expectedBank: SplendidResourceList = {
         black: 0,
         white: 0,
         red: 0,
         blue: 0,
         green: 0,
         yellow: 0,
-      });
+      };
+
+      expect(newState.game?.board.bankOffsetTemp).toEqual(expectedBank);
     });
 
     it("it returns an updated offset bank", () => {
-      const initialState: State = { ...defaultState, game: { ...game } };
-      const action: SplendidResourceAction = {
-        type: "UPDATE_BANK_RESOURCE",
-        payload: { red: 1, green: 1 },
-      };
+      const initialState: State = { ...defaultState, game };
+      const action = updateBankResources({ red: 1, green: 1 });
       const newState = reducer(initialState, action);
-      expect(newState.game?.board.bankOffsetTemp).toMatchObject({
+      const expectedBank: SplendidResourceList = {
         black: 0,
         white: 0,
         red: 1,
         blue: 0,
         green: 1,
         yellow: 0,
-      });
+      };
+
+      expect(newState.game?.board.bankOffsetTemp).toEqual(expectedBank);
     });
   });
 });
