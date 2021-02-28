395a16e9d6cf48f237221db1e2bf8ff6f6ea6739
vannio <vannio@users.noreply.github.com>
2021-02-28 21:40:29 +0000

    disabled start game for non firsts

--- client/src/components/Sidebar/ActionsForm/StartGameButton.tsx
+++ client/src/components/Sidebar/ActionsForm/StartGameButton.tsx
@@ -4,11 +4,22 @@ import Button from "../../common/Button";
 import { useGame } from "../../../hooks/useGame";
 import { useLobbyId } from "../../../hooks/useLobbyId";
 import { sendJSON } from "../../../hooks/useWebsocket";
+import { useSelector } from "react-redux";
+import { State } from "../../../state/domain";
 
 const StartGameButton = () => {
   const [lobbyId] = useLobbyId();
   const [game] = useGame();
-  if (!lobbyId || game) {
+  const { playerNames, clientId } = useSelector(
+    ({ playerNames, clientId }: State) => ({
+      playerNames,
+      clientId,
+    })
+  );
+
+  const isFirstJoiner = Object.keys(playerNames)[0] === clientId?.toString();
+
+  if (!lobbyId || game || !isFirstJoiner) {
     return null;
   }
   return (
